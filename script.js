d2330193ee746764d44a38c237a1ba91eac4b3c4
tanmayrana222 <tanmayraa@gmail.com>
2025-02-22 22:08:28 +0530

    updated

--- script.js
+++ script.js
@@ -7,17 +7,31 @@ const secondsElement = document.getElementById('seconds');
 const progressBar = document.querySelector('.progress');
 const startButton = document.getElementById('start');
 const resetButton = document.getElementById('reset');
+const inputHours = document.getElementById('inputHours') ;
+const inputMinutes = document.getElementById('inputMinutes');
+const inputSeconds = document.getElementById('inputSeconds');
 function startTimer(duration) {
-    let startTime = Date.now();
-    let endTime = startTime + duration * 1000;
+    if(timerInterval) clearInterval (timerInterval) ;
+
+    
+    totalTime = duration;
+    timeLeft = duration ;
+
+    const startTime = Date.now();
+    const endTime = startTime + duration * 1000;
     timerInterval = setInterval(() => {
         timeLeft = Math.max(0, endTime - Date.now());
         updateDisplay(timeLeft);
+        const elapsed = totalTime * 1000 - timeLeft;
+        const progressPercent = (elapsed/(totalTime*1000)) * 100 ;
+        progressBar.style.width = `${progressPercent}%`;
+
         if (timeLeft <= 0) {
             clearInterval(timerInterval);
+            progressBar.style.width = "100%" ;
             alert("Time's up!");
         }
-    }, 1000);
+    }, 50);
 }
 function updateDisplay(time) {
     const hours = Math.floor(time / (1000 * 60 * 60));
@@ -30,15 +44,36 @@ function updateDisplay(time) {
     progressBar.style.width = `${progressWidth}%`;
 }
 startButton.addEventListener('click', () => {
-    if (!totalTime) {
-        totalTime = 60; // Set default timer to 1 minute
+    const hours = parseInt(inputHours.value) || 0 ; 
+    const minutes = parseInt(inputMinutes.value) || 0 ;
+    const seconds = parseInt(inputSeconds.value) || 0 ;
+    totalTime = hours * 3600 + minutes * 60 + seconds ;
+    if (totalTime<= 0 ) {
+        alert("please enter a valid time ");
+        return;
     }
-    timeLeft = totalTime * 1000;
-    startTimer(totalTime);
-});
+    inputHours.disabled = true 
+    inputMinutes.disabled = true 
+    inputSeconds.disabled = true 
+    startTimer (totalTime);
+}); 
+
 resetButton.addEventListener('click', () => {
     clearInterval(timerInterval);
-    timeLeft = totalTime * 1000;
-    updateDisplay(timeLeft);
-    progressBar.style.width = '0%';
+    timeLeft = totalTime * 1000 ;
+    updateDisplay (timeLeft)
+    inputHours.disabled = false
+    inputMinutes.disabled = false
+    inputSeconds.disabled = false
+
+    hoursElement.textContent = "00";
+    MinutesElement.textContent = "00";
+    SecondsElement.textContent = "00";
+    
+    progressBar.style.width = "0%"
+    inputHours.value = ""
+    inputMinutes.value = ""
+    inputseconds.value = ""
+
+
 });
